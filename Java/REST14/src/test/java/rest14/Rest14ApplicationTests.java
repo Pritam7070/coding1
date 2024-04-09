1426fdc4265a237e882a1fe1a3720c221d8f42f1
Pritam Tripathy <103483761+Pritam7070@users.noreply.github.com>
2024-04-09 12:35:00 +0530

    Spring Security (Postman/Talend API)

---
+++ Java/REST14/src/test/java/rest14/Rest14ApplicationTests.java
@@ -0,0 +1,13 @@
+package rest14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
